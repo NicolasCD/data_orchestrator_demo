45525b48e393e384a8dc0d27adf6324f020a2da2
Nicolas <nicolas@saagie.com>
2026-07-10 15:42:45 +0200

    WIP

--- function_1/handlers/get_file.py
+++ function_1/handlers/get_file.py
@@ -6,7 +6,7 @@ import os
 def handle(event, context):
     source_url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
     s3_file_name = 'titanic.csv'
-    bucket_name = 'titanic'
+    bucket_name = 'example-titanic'
     
     response = requests.get(source_url, stream=True)
     try:
@@ -28,9 +28,16 @@ def handle(event, context):
                 },
                 "statusCode": 400,
             }        
+    except Exception as e:
+        return {
+            "body": {
+                "message": f"An error occurred: {e}",
+            },
+            "statusCode": 400,
+        }        
     return {
         "body": {
-            "message": "Titanic bucket well created",
+            "message": '"example-titanic" bucket well created'
         },
         "statusCode": 200,
     }
--- script.sh
+++ script.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-AWS_ACCESS_KEY_ID='SCWZXQANBYN3YSQ9MQR8'
-AWS_SECRET_ACCESS_KEY='b43c3ec5-998d-4677-80ba-491ac8cbb1ec'
+AWS_ACCESS_KEY_ID=''
+AWS_SECRET_ACCESS_KEY=''
 AWS_DEFAULT_REGION='fr-par'
 AWS_ENDPOINT_URL='http://s3.fr-par.scw.cloud'
+
+wd=$(pwd)
 
 echo namespace creating
 #Create namespace
 NAMESPACE=$(scw function namespace create | grep 'ID' | head -n1 | tr -s ' ' | cut -d' ' -f2)
-#Wait creating namespace
 sleep 5
+echo namespace ID $NAMESPACE created
 
 #Function_1
 echo function_1 creating
-cd function_1
+f=function_1
+cd "$wd/$f"
 PYTHON_VERSION=3.13 && docker run --rm -v .:/home/app/function --workdir /home/app/function rg.fr-par.scw.cloud/scwfunctionsruntimes-public/python-dep:$PYTHON_VERSION pip install -r requirements.txt --target ./package
 
 #Compress archive
-zip -r function_1.zip handlers package
+zip -r $f.zip handlers package
 
 #Size of archive
-CONTENT_LENGTH=$(du -b function_1.zip | cut -f1)
+CONTENT_LENGTH=$(du -b $f.zip | cut -f1)
 
 #Create function. Python 3.14 not dispo with "scw" cli
 ID=$(scw function function create \
@@ -28,7 +31,8 @@ namespace-id=$NAMESPACE \
 runtime=python313 \
 handler='handlers/get_file.handle' \
 environment-variables.AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-environment-variables.AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+secret-environment-variables.0.key=AWS_SECRET_ACCESS_KEY \
+secret-environment-variables.0.value=$AWS_SECRET_ACCESS_KEY \
 environment-variables.AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
 environment-variables.AWS_ENDPOINT_URL=$AWS_ENDPOINT_URL \
  | grep 'ID' | head -n1 | tr -s ' ' | cut -d' ' -f2)
@@ -40,7 +44,7 @@ URL=$(scw function function get-upload-url $ID content-length=$CONTENT_LENGTH | 
 curl $URL \
 -X PUT \
 -H 'Content-Type: application/octet-stream' \
---data-binary @function_1.zip
+--data-binary @$f.zip
 
 #Deploy
 scw function function deploy $ID
@@ -50,7 +54,57 @@ while true; do
     STATUS=$(scw function function get $ID | grep 'Status' | head -n1 | tr -s ' ' | cut -d' ' -f2)
 
     if [ "$STATUS" = "ready" ]; then
-        echo Waiting deploying function_1
+        echo $f deployed
+        break
+    else
+        sleep 2
+    fi
+done
+
+
+
+#Function_2
+echo function_2 creating
+f=function_2
+cd "$wd/$f"
+PYTHON_VERSION=3.13 && docker run --rm -v .:/home/app/function --workdir /home/app/function rg.fr-par.scw.cloud/scwfunctionsruntimes-public/python-dep:$PYTHON_VERSION pip install -r requirements.txt --target ./package
+
+#Compress archive
+zip -r $f.zip handlers package
+
+#Size of archive
+CONTENT_LENGTH=$(du -b $f.zip | cut -f1)
+
+#Create function. Python 3.14 not dispo with "scw" cli
+ID=$(scw function function create \
+namespace-id=$NAMESPACE \
+runtime=python313 \
+handler='handlers/compute_file.handle' \
+environment-variables.AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+secret-environment-variables.0.key=AWS_SECRET_ACCESS_KEY \
+secret-environment-variables.0.value=$AWS_SECRET_ACCESS_KEY \
+environment-variables.AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+environment-variables.AWS_ENDPOINT_URL=$AWS_ENDPOINT_URL \
+ | grep 'ID' | head -n1 | tr -s ' ' | cut -d' ' -f2)
+
+#Asking for upload URL
+URL=$(scw function function get-upload-url $ID content-length=$CONTENT_LENGTH | grep 'URL' | head -n1 | tr -s ' ' | cut -d' ' -f2)
+
+#Upload archive
+curl $URL \
+-X PUT \
+-H 'Content-Type: application/octet-stream' \
+--data-binary @$f.zip
+
+#Deploy
+scw function function deploy $ID
+
+#Wait status is "ready"
+while true; do
+    STATUS=$(scw function function get $ID | grep 'Status' | head -n1 | tr -s ' ' | cut -d' ' -f2)
+
+    if [ "$STATUS" = "ready" ]; then
+        echo $f deployed
         break
     else
         sleep 2
